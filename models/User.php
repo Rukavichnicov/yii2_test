52e77427b815d98d5cda4bb73c1bf5965c0ca482
Sergey Rukavichnikov <serega1998love@yandex.ru>
2023-01-29 15:46:52 +0500

    Added rules validations in class User

--- models/User.php
+++ models/User.php
@@ -14,8 +14,30 @@ class User extends ActiveRecord
     {
         return '{{user}}';
     }
+
     public function fields(): array
     {
         return ['id', 'name', 'surname', 'image'];
+    }
+
+    public function rules()
+    {
+        return [
+            ['name', 'required', 'message' => 'Пожалуйста введите имя пользователя'],
+            ['password', 'required', 'message' => 'Пожалуйста введите пароль'],
+            ['password_repeat', 'required', 'message' => 'Пожалуйста введите повтор пароля'],
+
+            ['password', 'compare', 'compareAttribute' => 'password_repeat', 'message' => 'Пароль и его повтор должны совпадать'],
+
+            ['name', 'string', 'max' => 100, 'message' => 'Имя должно быть строкой не более 100 символов'],
+            ['surname', 'string', 'max' => 100, 'message' => 'Фамилия должна быть строкой не более 100 символов'],
+            ['password', 'string', 'max' => 100, 'message' => 'Пароль должен быть строкой не более 100 символов'],
+            ['password_repeat', 'string', 'max' => 100, 'message' => 'Повтор пароля должен быть строкой не более 100 символов'],
+
+            [['image'], 'image', 'maxSize' => 1024 * 1024 * 10, 'message' => 'Загружаемый файл должен быть картинкой, размером не более 10 МB'],
+
+            ['surname', 'default', 'value' => null],
+            [['name', 'surname'], 'trim'],
+        ];
     }
 }
